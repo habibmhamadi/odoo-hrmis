d0576a7f0ad32b7867ef91a632617dcfa2505d32
habibmhamadi <habibmhamadi@gmail.com>
2023-05-17 16:19:50 +0430

    Scaffold contract module.

---
+++ hm_contract/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contract",
+
+    'summary': """
+        Manage employee contract""",
+
+    'description': """
+        Manage employee contract
+    """,
+
+    'author': "HabibMhamadi",
+    'website': "https://www.github.com/habibmhamadi",
+    'category': 'Employees',
+    'version': '0.1',
+
+    'depends': ['base', 'mail', 'resource', 'hm_employee'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'application': True
+}
